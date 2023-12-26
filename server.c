0176ffcc22978843548848b62fb3287fbcafb39a
MunJunHo <mjh1995713@gmail.com>
2023-12-26 11:46:00 +0900

    ubunto 에서 clone, gitignore 업데이트

---
+++ server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#define PORT 3490
+#define BACKLOG 10
+
+int main()
+{
+    int sockfd, newfd;
+}
